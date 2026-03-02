187f3cdc5e9cb0ebafdb80a6d552a25041ad66fd
Rémi <139657357+RemiAsselin42@users.noreply.github.com>
2026-03-02 22:16:22 +0100

    feat(styles): migrer la base visuelle vers SCSS
    Remplace l’ancien style CSS minimal par une structure SCSS avec variables et mixins réutilisables.
    
    Met à jour l’entrée principale pour charger le nouveau stylesheet et prépare le terrain pour les composants visuels du projet.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
-import './style.css'
-import * as THREE from 'three'
-import { gsap } from 'gsap'
+import './style.scss';
+import * as THREE from 'three';
+import { gsap } from 'gsap';
+
+// TODO: Implémenter la scène 3D avec Three.js et les animations GSAP
+// eslint-disable-next-line no-console
+console.log('Luv Resval Tribute - Initialisation...', { THREE, gsap });
