786cc1a90f8631cf89beddebb9fb204a95dbf36e
Rémi <139657357+RemiAsselin42@users.noreply.github.com>
2026-03-02 23:57:10 +0100

    feat(config): ajouter les types Node et configurer la base pour Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 
+const repository = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const base = repository ? `/${repository}/` : '/';
+
 export default defineConfig({
+  base,
   resolve: {
     alias: {
       '@': '/src',
